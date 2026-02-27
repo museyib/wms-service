6d62eb9f6931e31a225a4527b6abc88907b2e55d
Museyib <museyib.e@gmail.com>
2026-02-27 11:10:24 +0400

    Bugfix

--- src/main/java/az/inci/wms/model/v4/ProductApproveRequestItem.java
+++ src/main/java/az/inci/wms/model/v4/ProductApproveRequestItem.java
@@ -1,10 +1,8 @@
 package az.inci.wms.model.v4;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class ProductApproveRequestItem {
     private String invCode;
     private String invName;
